21a8be216a3a78dc32b8fbc404ea3bcb8f703b95
Trina Lu <tri613@gmail.com>
2019-01-25 17:52:41 +0800

    Make snake controllable by user

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log('cooool!');
+import Snake from './snake';
+
+const UNIT = 20;
+
+const canvas = document.querySelector('#game');
+const ctx = canvas.getContext('2d');
+canvas.width = UNIT * 30;
+canvas.height = UNIT * 30;
+
+const snake = new Snake(UNIT);
+
+const drawSnake = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  snake.body.forEach(({ x, y }) => {
+    ctx.strokeRect(x, y, UNIT, UNIT);
+  });
+};
+
+function draw() {
+  requestAnimationFrame(draw);
+  drawSnake();
+}
+
+document.addEventListener('keydown', e => {
+  switch (e.code) {
+    case 'ArrowDown':
+      snake.moveDown();
+      break;
+
+    case 'ArrowUp':
+      snake.moveUp();
+      break;
+
+    case 'ArrowRight':
+      snake.moveRight();
+      break;
+
+    case 'ArrowLeft':
+      snake.moveLeft();
+      break;
+
+    default:
+      break;
+  }
+});
+
+setInterval(() => snake.moveForward(), 500);
+draw();
