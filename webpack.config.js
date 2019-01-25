bbf95aa04be9a75e185d174a43095c9ae39fb6c1
Trina Lu <tri613@gmail.com>
2019-01-25 16:43:46 +0800

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Snakkkkkkke!'
+    })
+  ]
+};
