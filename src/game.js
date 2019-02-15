272ec14c79a38c0f499a44dde1960ddf104edc51
Trina Lu <tri613@gmail.com>
2019-02-15 19:08:23 +0800

    途中です

--- src/game.js
+++ src/game.js
@@ -2,15 +2,49 @@ import { interval, fromEvent, merge, of } from 'rxjs';
 import { map, bufferTime, mapTo, skipUntil, tap, take } from 'rxjs/operators';
 import Snake from './snake';
 
-export function createGame(unit, boundary) {
-  const snake = new Snake(unit);
+function hitBoundary(x, y, boundary) {
+  return x < 0 || x >= boundary || y < 0 || y >= boundary;
+}
+
+function grow(tail, butt, unit) {
+  const { x: tx, y: ty } = tail;
+  const { x: bx, y: by } = butt;
+
+  if (tx < bx) {
+    return { x: tx - unit, y: ty };
+  }
+
+  if (tx > bx) {
+    return { x: tx + unit, y: ty };
+  }
+
+  if (by > ty) {
+    return { x: tx, y: ty - unit };
+  }
+
+  if (by < ty) {
+    return { x: tx, y: ty + unit };
+  }
+}
+
+export function createGame(UNIT, BOUNDARY) {
+  const snake = new Snake(UNIT);
 
   const init = of(snake.body);
   const keydowns = fromEvent(document, 'keydown').pipe(
     map(e => e.code.replace('Arrow', 'move'))
   );
   const autoMove = interval(1000).pipe(mapTo('moveForward'));
-  const grow = interval(300).pipe(mapTo('grow'));
+
+  const grow = interval(300).pipe(
+    map(() => {
+      let newTail;
+      newTail = grow(snake.tail, snake.body[snake.body.length - 2], UNIT);
+
+      if (x > BOUNDARY) {
+      }
+    })
+  );
 
   const listeners = merge(keydowns, autoMove, grow).pipe(
     skipUntil(keydowns),
@@ -31,13 +65,8 @@ export function createGame(unit, boundary) {
     map(body => {
       const [head, ...rest] = body;
 
-      if (
-        head.x < 0 ||
-        head.x >= boundary ||
-        head.y < 0 ||
-        head.y >= boundary
-      ) {
-        throw '撞到自己辣！';
+      if (hitBoundary(head.x, head.y, BOUNDARY)) {
+        throw '撞到牆辣！';
       }
 
       if (rest.some(({ x, y }) => head.x === x && head.y === y)) {
